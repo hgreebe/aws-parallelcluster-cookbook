59c049a2c861a0f62644392d92bc3086c4fa9311
Helena Greebe <hgreebe@amazon.com>
2024-02-02 10:16:01 -0500

    back to og bucket

--- cookbooks/aws-parallelcluster-shared/attributes/environment.rb
+++ cookbooks/aws-parallelcluster-shared/attributes/environment.rb
@@ -5,4 +5,4 @@ default['cluster']['aws_domain'] = aws_domain
 
 # URL for ParallelCluster Artifacts stored in public S3 buckets
 # ['cluster']['region'] will need to be defined by image_dna.json during AMI build.
-default['cluster']['artifacts_s3_url'] = "https://aws-parallelcluster-dev-commercial.s3.#{node['cluster']['region']}.#{node['cluster']['aws_domain']}/archives"
+default['cluster']['artifacts_s3_url'] = "https://#{node['cluster']['region']}-aws-parallelcluster.s3.#{node['cluster']['region']}.#{node['cluster']['aws_domain']}/archives"
