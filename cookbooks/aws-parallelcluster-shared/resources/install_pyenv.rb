115ed8a9c9c98f94dab374255f8e5d2b74e824be
Helena Greebe <hgreebe@amazon.com>
2024-06-06 13:17:28 -0400

    Fix pyenv

--- cookbooks/aws-parallelcluster-shared/resources/install_pyenv.rb
+++ cookbooks/aws-parallelcluster-shared/resources/install_pyenv.rb
@@ -4,12 +4,23 @@ provides :install_pyenv
 unified_mode true
 
 # Resource:: to create a Python virtual environment for a given user
-
-property :python_version, String
-property :prefix, String
+property :home_dir,
+         String,
+         default: lazy { ::File.expand_path("~#{user}") }
 property :user_only, [true, false], default: false
 property :user, String
-
+property :prefix,
+         String,
+         default: lazy {
+           if user_only
+             ::File.join(home_dir, '.pyenv')
+           else
+             '/usr/local/pyenv'
+           end
+         },
+         description: 'Path to install pyenv to'
+property :python_version, String
+property :prefix, String
 default_action :run
 
 action :run do
@@ -18,39 +29,36 @@ action :run do
   if new_resource.user_only
     raise "user property is required for resource install_pyenv when user_only is set to true" unless new_resource.user
 
-    pyenv_install 'user' do
-      user new_resource.user
-      prefix new_resource.prefix if new_resource.prefix
-    end
   else
     prefix = new_resource.prefix || node['cluster']['system_pyenv_root']
+  end
 
-    directory prefix do
-      recursive true
-    end
+  directory prefix do
+    recursive true
+  end
 
-    remote_file "#{prefix}/Python-#{python_version}.tgz" do
-      source "#{node['cluster']['artifacts_s3_url']}/dependencies/python/Python-#{python_version}.tgz"
-      mode '0644'
-      retries 3
-      retry_delay 5
-      action :create_if_missing
-    end
+  remote_file "#{prefix}/Python-#{python_version}.tgz" do
+    source "#{node['cluster']['artifacts_s3_url']}/dependencies/python/Python-#{python_version}.tgz"
+    mode '0644'
+    retries 3
+    retry_delay 5
+    action :create_if_missing
+  end
 
-    bash "install python #{python_version}" do
-      user 'root'
-      group 'root'
-      cwd "#{prefix}"
-      code <<-VENV
-      set -e
-      tar -xzf Python-#{python_version}.tgz 
-      cd Python-#{python_version}
-      ./configure --prefix=#{prefix}/versions/#{python_version}
-      make
-      make install
-      VENV
-    end
+  user = new_resource.user || 'root'
 
+  bash "install python #{python_version}" do
+    user user
+    group 'root'
+    cwd "#{prefix}"
+    code <<-VENV
+    set -e
+    tar -xzf Python-#{python_version}.tgz 
+    cd Python-#{python_version}
+    ./configure --prefix=#{prefix}/versions/#{python_version}
+    make
+    make install
+    VENV
   end
 
 end
