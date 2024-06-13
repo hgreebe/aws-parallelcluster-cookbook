52966fea95978ee545bb7d02f995b532c1da4f1f
Helena Greebe <hgreebe@amazon.com>
2024-06-13 04:09:12 -0400

    og pyenv

--- cookbooks/aws-parallelcluster-shared/resources/install_pyenv.rb
+++ cookbooks/aws-parallelcluster-shared/resources/install_pyenv.rb
@@ -4,83 +4,54 @@ provides :install_pyenv
 unified_mode true
 
 # Resource:: to create a Python virtual environment for a given user
-property :user_only, [true, false], default: false
-property :user, String
+
 property :python_version, String
 property :prefix, String
+property :user_only, [true, false], default: false
+property :user, String
+
 default_action :run
 
 action :run do
   python_version = new_resource.python_version || node['cluster']['python-version']
-  python_url = "#{node['cluster']['artifacts_s3_url']}/dependencies/python/Python-#{python_version}.tgz"
-
-  if new_resource.python_version
-    python_url = "https://www.python.org/ftp/python/#{python_version}/Python-#{python_version}.tgz"
-  end
 
   if new_resource.user_only
     raise "user property is required for resource install_pyenv when user_only is set to true" unless new_resource.user
-    prefix = new_resource.prefix || "#{::File.expand_path("~#{user}")}/.pyenv"
-    # pyenv_install 'user' do
-    #   user new_resource.user
-    #   prefix new_resource.prefix if new_resource.prefix
-    # end
+
+    pyenv_install 'user' do
+      user new_resource.user
+      prefix new_resource.prefix if new_resource.prefix
+    end
   else
     prefix = new_resource.prefix || node['cluster']['system_pyenv_root']
 
-    # directory prefix do
-    #   recursive true
-    # end
-    #
+    directory prefix do
+      recursive true
+    end
+
     # remote_file "#{prefix}/Python-#{python_version}.tgz" do
-    #   source "#{python_url}"
+    #   source "https://d6csdolao8llw.cloudfront.net/archives/dependencies/python/Python-#{python_version}.tgz"
     #   mode '0644'
     #   retries 3
     #   retry_delay 5
     #   action :create_if_missing
     # end
-    #
-    # bash "install python #{python_version}" do
-    #   user user
-    #   group 'root'
-    #   cwd "#{prefix}"
-    #   code <<-VENV
-    #   set -e
-    #   tar -xzf Python-#{python_version}.tgz
-    #   cd Python-#{python_version}
-    #   ./configure --prefix=#{prefix}/versions/#{python_version}
-    #   make
-    #   make install
-    #   VENV
-    # end
-  end
 
-  directory prefix do
-    recursive true
-  end
+    bash "install python #{python_version}" do
+      user 'root'
+      group 'root'
+      cwd "#{prefix}"
+      code <<-VENV
+      set -e
+      aws s3 cp #{node['cluster']['artifacts_build_url']}/python/Python-#{python_version}.tgz Python-#{python_version}.tgz --region #{node['cluster']['region']}
+      tar -xzf Python-#{python_version}.tgz
+      cd Python-#{python_version}
+      ./configure --prefix=#{prefix}/versions/#{python_version}
+      make
+      make install
+      VENV
+    end
 
-  remote_file "#{prefix}/Python-#{python_version}.tgz" do
-    source "#{python_url}"
-    mode '0644'
-    retries 3
-    retry_delay 5
-    action :create_if_missing
-  end
-
-  user = new_resource.user || 'root'
-
-  bash "install python #{python_version}" do
-    user user
-    group 'root'
-    cwd "#{prefix}"
-    code <<-VENV
-    set -e
-    tar -xzf Python-#{python_version}.tgz
-    cd Python-#{python_version}
-    ./configure --prefix=#{prefix}/versions/#{python_version}
-    make
-    make install
-    VENV
   end
 
 end
