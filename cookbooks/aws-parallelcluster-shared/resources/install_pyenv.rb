6beb817b9d004c6569546738dcdc098b3f5f09a0
Helena Greebe <hgreebe@amazon.com>
2024-06-13 08:11:30 -0400

    python through https

--- cookbooks/aws-parallelcluster-shared/resources/install_pyenv.rb
+++ cookbooks/aws-parallelcluster-shared/resources/install_pyenv.rb
@@ -29,13 +29,13 @@ action :run do
       recursive true
     end
 
-    # remote_file "#{prefix}/Python-#{python_version}.tgz" do
-    #   source "https://d6csdolao8llw.cloudfront.net/archives/dependencies/python/Python-#{python_version}.tgz"
-    #   mode '0644'
-    #   retries 3
-    #   retry_delay 5
-    #   action :create_if_missing
-    # end
+    remote_file "#{prefix}/Python-#{python_version}.tgz" do
+      source "#{node['cluster']['artifacts_s3_url']}/dependencies/python/Python-#{python_version}.tgz"
+      mode '0644'
+      retries 3
+      retry_delay 5
+      action :create_if_missing
+    end
 
     bash "install python #{python_version}" do
       user 'root'
@@ -43,7 +43,6 @@ action :run do
       cwd "#{prefix}"
       code <<-VENV
       set -e
-      aws s3 cp #{node['cluster']['artifacts_build_url']}/python/Python-#{python_version}.tgz Python-#{python_version}.tgz --region #{node['cluster']['region']}
       tar -xzf Python-#{python_version}.tgz
       cd Python-#{python_version}
       ./configure --prefix=#{prefix}/versions/#{python_version}
