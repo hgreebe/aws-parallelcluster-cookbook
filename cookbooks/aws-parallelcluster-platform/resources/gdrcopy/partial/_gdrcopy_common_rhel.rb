5148aecd57fec2b313a704eb1d1238bb9ce53ef4
Helena Greebe <hgreebe@amazon.com>
2024-06-25 14:16:04 -0400

    [RHEL8 EPEL PACKAGES] remove packages not found adc regions

--- cookbooks/aws-parallelcluster-platform/resources/gdrcopy/partial/_gdrcopy_common_rhel.rb
+++ cookbooks/aws-parallelcluster-platform/resources/gdrcopy/partial/_gdrcopy_common_rhel.rb
@@ -18,7 +18,7 @@ end
 
 def gdrcopy_build_dependencies
   # %w(dkms rpm-build make check check-devel subunit subunit-devel)
-  %w(dkms rpm-build make check check-devel)
+  %w(rpm-build make check check-devel)
 end
 
 def installation_code
--- cookbooks/aws-parallelcluster-platform/resources/install_packages/install_packages_redhat8.rb
+++ cookbooks/aws-parallelcluster-platform/resources/install_packages/install_packages_redhat8.rb
@@ -23,12 +23,20 @@ def default_packages
   # environment-modules required by EFA, Intel MPI and ARM PL
   # Removed libssh2-devel from base_packages since is not shipped by RedHat 8 and in conflict with package libssh-0.9.6-3.el8.x86_64
   # iptables needed for IMDS setup
-  %w(vim ksh tcsh zsh openssl-devel ncurses-devel pam-devel net-tools openmotif-devel
-     libXmu-devel hwloc-devel libdb-devel tcl-devel automake autoconf libtool
+  # %w(vim ksh tcsh zsh openssl-devel ncurses-devel pam-devel net-tools openmotif-devel
+  #    libXmu-devel hwloc-devel libdb-devel tcl-devel automake autoconf libtool
+  #    httpd boost-devel mlocate R atlas-devel
+  #    blas-devel libffi-devel dkms libedit-devel jq
+  #    libical-devel sendmail libxml2-devel libglvnd-devel
+  #    libgcrypt-devel libevent-devel glibc-static bind-utils
+  #    iproute NetworkManager-config-routing-rules python3 python3-pip iptables libcurl-devel yum-plugin-versionlock
+  #    coreutils moreutils curl environment-modules gcc gcc-c++ bzip2)
+  %w(vim ksh tcsh zsh openssl-devel ncurses-devel pam-devel net-tools
+     libXmu-devel libdb-devel tcl-devel automake autoconf libtool
      httpd boost-devel mlocate R atlas-devel
-     blas-devel libffi-devel dkms libedit-devel jq
+     libffi-devel jq
      libical-devel sendmail libxml2-devel libglvnd-devel
-     libgcrypt-devel libevent-devel glibc-static bind-utils
-     iproute NetworkManager-config-routing-rules python3 python3-pip iptables libcurl-devel yum-plugin-versionlock
-     coreutils moreutils curl environment-modules gcc gcc-c++ bzip2)
+     libgcrypt-devel libevent-devel bind-utils
+     iproute python3 python3-pip iptables libcurl-devel
+     coreutils curl environment-modules gcc gcc-c++ bzip2)
 end
